92e45756d6a42b90f7a371f24a97b05426586340
TsukinoseTillot <fatilla97@gmail.com>
2018-01-13 00:08:32 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,22 +11,12 @@ client.on('message', message => {
     	message.reply('Hii!!');
   	}
     
-    {
-        client.on('ready', () => {
-  console.log('I am ready!');
 });
 
-// Create an event listener for new guild members
-client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find('name', 'member-log');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
-});
-
-    }
+client.on('message', message => {
+    if (message.content === 'bye') {
+    	message.reply('Bye !!!!');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
