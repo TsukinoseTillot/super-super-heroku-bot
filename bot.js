510c7502b06bb921b0353809d9cf70642e6b27b5
TsukinoseTillot <fatilla97@gmail.com>
2018-01-13 00:11:46 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === 'bye') {
-    	message.reply('Bye !!!!');
+    	message.channel.send('Bye !!!!');
   	}
 });
 
