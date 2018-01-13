184cf02c439970b1f6abf6ed9fed70feb4377f44
TsukinoseTillot <fatilla97@gmail.com>
2018-01-13 22:05:51 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,6 +86,11 @@ client.on("message", message => {
 
 });
   
+client.on('message', message => {
+    if(message.content.startsWith("!ping")) {
+            message.channel.send(new Date().getTime() - message.createdTimestamp + "ms");        
+    }
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
