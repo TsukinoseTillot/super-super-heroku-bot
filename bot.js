ceb986f575f873949b19974ec77564d8bb50f4a7
TsukinoseTillot <fatilla97@gmail.com>
2018-01-14 18:37:55 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,97 +1,95 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-const swearWords = ["fuck", "asshole","shit", "bullshit, faggot"];
-const prefix = "!";
-const newUsers = [];
+const Discord = require("discord.js");
+const client = new Discord.Client({disableEveryone: true});
+const prefix = "!!"
+const swearWords = ["fuck", "asshole","shit", "bullshit", "motherfucker", "cocksucker", "cunt", "tits"];
 
 
-client.on("ready", () => {
-  console.log("I am ready!");
+client.on("ready", async () => {
+  console.log("yes im ready!");
 });
+
 client.on("ready", () => {
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.setGame(`with ${client.guilds.size} servers, ${client.users.size} users`);
   console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`);
 });
 
-client.on("guildMemberAdd", member => {
-   member.guild.defaultChannel.send("Welcome " + member.user.username)
-});
-
-client.on("guildMemberRemove", member => {
-   member.guild.defaultChannel.send("Goodbye:" + member.user.username + ".See you next time")
-});
-
-
-client.on("message", (message) => {
-  if (message.content.startsWith("owo")) {
-    message.channel.send("What's this!");
-  }
-});
-
-client.on("message", (message) => {
-  if (!message.content.startsWith(prefix)) return;
-
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.startsWith(prefix + "foo")) {
-    message.channel.send("bar!");
-  }
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
   
-});
-  client.on("message", (message) => {
-  if (!message.content.startsWith(prefix)) return;
-
-  if (message.content.startsWith(prefix + "avatar")) {
-    message.channel.send(message.author.avatarURL);
-  }
-});
-
-client.on("message", (message) => {  
-if( swearWords.some(word => message.content.includes(word)) ) {
-  message.reply("Oh no you said a bad word!!!");
-  message.delete();
-  }
-});
-
-client.on("message", message => {
-
-  if(!message.content.startsWith(prefix)) return;
+  let messageArray = message.content.split(" ");
+  let command = messageArray[0];
+  let args = messageArray.slice(1);
   
-  if(message.content.startsWith(prefix + "userinfo")){
-    let embed = new Discord.RichEmbed()
+  if(!command.startsWith(prefix)) return;
+  
+  if(command === `${prefix}userinfo`) {
+      let embed = new Discord.RichEmbed()
     .setAuthor(message.author.username)
-    .setDescription("Userinfo")
     .setColor("AF5AFF")
     .addField("Full Username", `${message.author.username}#${message.author.discriminator}`)
     .addField("ID", message.author.id)
     .addField("Created At", message.author.createdAt);
     
-  message.channel.sendEmbed(embed);
+  message.channel.send(embed);
   }
 
-});
-  
-client.on('message', message => {
-    if(message.content.startsWith("!ping")) {
-            message.channel.send(new Date().getTime() - message.createdTimestamp + "ms");        
-    }
-});
-
-client.on("message", message => {
-
-  if(!message.content.startsWith(prefix)) return;
-
- if(message.content.startsWith(prefix + "about")){
+   if(message.content.startsWith(prefix + "about")){
     let embed = new Discord.RichEmbed()
     .setAuthor("Tsukibot")
     .setDescription("This bot is for personal use only.. not public user")
     .setColor("3DB6F3");
    message.channel.send(embed);
  }
+  
 });
 
+ client.on("message", async (message) => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+	
+	let command = message.content.split(" ")[0];
+	command = command.slice(prefix.length);
+	
+	let args = message.content.split(" ").slice(1);
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+if(command === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+     if(command === "sayd") {
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(sayMessage);
+  }
+        if(command === "say") {
+    const sayMessage = args.join(" ");
+    message.channel.send(sayMessage);
+  }
+     if(command === "prune") {
+    const deleteCount = parseInt(args[0], 10);
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
+    });
+
+client.on("message", (message) => {
+  if(message.author.bot) return;
+     if (message.content.startsWith("owo")) {
+    message.channel.send("What's this!");
+  }
+  
+  client.on("message", (message) => {  
+if( swearWords.some(word => message.content.includes(word)) ) {
+  message.reply("Oh no you said a bad word!!!");
+  message.delete();
+  }
+});
+
+  
+  
+});
+client.login("NDAxOTYzMDE4OTczMzQ3ODQw.DTx0xA.TWm1buIcLWRTvqA8EoSctS2b8pU");
