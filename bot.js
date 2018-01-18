3a5716b77ad8bc1ce49bcaf07f18846e2beb8931
TsukinoseTillot <fatilla97@gmail.com>
2018-01-18 14:33:49 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,142 @@
 const Discord = require("discord.js");
-const client = new Discord.Client({disableEveryone: true});
-const prefix = "!"
 
-process.setMaxListeners(0);
+const client = new Discord.Client();
+const config = require("./config.json");
 
-client.on("ready", async () => {
-  console.log("yes im ready!");
-});
+const sql = require("sqlite");
+sql.open("./score.sqlite");
 
 client.on("ready", () => {
-  client.user.setGame(`with ${client.guilds.size} servers, ${client.users.size} users`);
-  console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`);
+ 
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+
+  client.user.setActivity(`on ${client.guilds.size} servers`);
 });
 
+client.on("guildCreate", guild => {
+
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  if(command === "ping") {
+
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+  
+  if(command === "say") {
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{}); 
+    message.channel.send(sayMessage);
+  }
+  
+  if(command === "kick") {
+    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+    
+
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the kick!");
+
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+
+  }
+  
+  if(command === "ban") {
+
+    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.bannable) 
+      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the ban!");
+    
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+  }
+  
+  if(command === "purge") {
+
+    const deleteCount = parseInt(args[0], 10);
+    
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+}
+
+  
+
+   sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+    if (!row) {
+      sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
+    } else {
+      let curLevel = Math.floor(0.1 * Math.sqrt(row.points + 1));
+      if (curLevel > row.level) {
+        row.level = curLevel;
+        sql.run(`UPDATE scores SET points = ${row.points + 1}, level = ${row.level} WHERE userId = ${message.author.id}`);
+        message.reply(`You've leveled up to level **${curLevel}**!`);
+      }
+      sql.run(`UPDATE scores SET points = ${row.points + 1} WHERE userId = ${message.author.id}`);
+    }
+  }).catch(() => {
+    console.error;
+    sql.run("CREATE TABLE IF NOT EXISTS scores (userId TEXT, points INTEGER, level INTEGER)").then(() => {
+      sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
+    });
+  });
+  
+    if (message.content.startsWith(prefix + "level")) {
+    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+      if (!row) return message.reply("Your current level is 0");
+      message.reply(`Your current level is ${row.level}`);
+    });
+  } else
+
+  if (message.content.startsWith(prefix + "points")) {
+    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
+      if (!row) return message.reply("sadly you do not have any points yet!");
+      message.reply(`you currently have ${row.points} points, keep going!`);
+    });
+  }
+  
+}
+          
+          
+         );
+const prefix = "+";
 client.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
@@ -21,7 +145,7 @@ client.on("message", async message => {
   let command = messageArray[0];
   let args = messageArray.slice(1);
   
-  if(!command.startsWith(prefix)) return;
+   if(!command.startsWith(prefix)) return;
   
   if(command === `${prefix}userinfo`) {
       let embed = new Discord.RichEmbed()
@@ -35,83 +159,12 @@ client.on("message", async message => {
   message.channel.send(embed);
   }
 
-   if(message.content.startsWith(prefix + "about")){
-    let embed = new Discord.RichEmbed()
-    .setAuthor("Tsukibot")
-    .setDescription("This bot is for personal use only.. not public user")
-    .setColor("3DB6F3");
-   message.channel.send(embed);
- }
-  
 });
 
- client.on("message", async (message) => {
-	if (message.author.bot) return;
-	if (!message.content.startsWith(prefix)) return;
-	
-	let command = message.content.split(" ")[0];
-	command = command.slice(prefix.length);
-	
-	let args = message.content.split(" ").slice(1);
-
-if(command === "ping") {
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-     if(command === "sayd") {
-    const sayMessage = args.join(" ");
-    message.delete().catch(O_o=>{}); 
-    message.channel.send(sayMessage);
-  }
-        if(command === "say") {
-    const sayMessage = args.join(" ");
-    message.channel.send(sayMessage);
-  }
-     if(command === "prune") {
-    const deleteCount = parseInt(args[0], 10);
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    const fetched = await message.channel.fetchMessages({count: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-  }
-    });
-
 client.on("message", (message) => {
-  if(message.author.bot) return;
      if (message.content.startsWith("owo")) {
     message.channel.send("What's this!");
   }
   });
-  
-  client.on("message", (message) => {
-  if(message.author.bot) return;
-     if (message.content.startsWith("OwO")) {
-    message.channel.send("What's this!");
-  }
-  
-   });
-  client.on("message", (message) => {
-  if(message.author.bot) return;
-     if (message.content.startsWith("Owo")) {
-    message.channel.send("What's this!");
-  }
-  
-  client.on("message", (message) => {
-  if(message.author.bot) return;
-     if (message.content.startsWith("owO")) {
-    message.channel.send("What's this!");
-  }
-   });
-    
-    client.on("message", (message) => {
-  if(message.author.bot) return;
-     if (message.content.startsWith("OWO")) {
-    message.channel.send("What's this!");
-  }
 
-    });
-  
-  
-});
 client.login(process.env.BOT_TOKEN);
