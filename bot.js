2a51db4b74e49cfdec45770c2aa6100a6f702f29
TsukinoseTillot <fatilla97@gmail.com>
2018-01-24 17:24:26 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on("ready", () => {
  
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
 
-  client.user.setActivity(`Shutdown and restart`);
+  client.user.setActivity(`See you tomorrow`);
 });
 
 client.on("guildCreate", guild => {
