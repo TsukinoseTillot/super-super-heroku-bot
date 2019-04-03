aa103fc67abbec21e740f4a5a47f2444788b34eb
TsukinoseTillot <fatilla97@gmail.com>
2019-04-03 14:19:55 +0800

    update status lol
    not important honestly

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on("ready", () => {
  
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
 
-  client.user.setActivity(`Watching ${client.users.size} users and do nothing`);
+  client.user.setActivity(`dead lol`);
 });
 
 
