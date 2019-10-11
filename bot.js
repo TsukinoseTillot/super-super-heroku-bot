15198d3a0c4a69b47256ac07bad4ea7472df5f9c
TsukinoseTillot <fatilla97@gmail.com>
2019-10-11 19:17:43 +0800

    changed "set activity"

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on("ready", () => {
  
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
 
-  client.user.setActivity(`dead lol`);
+  client.user.setActivity(`Don't ask what happened`);
 });
 
 
